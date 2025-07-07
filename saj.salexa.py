c4e0027ce0eeb8f17d207593e77abdc1467ba072
Ssajeetha p <sajeetha31@gmail.com>
2025-07-07 22:41:16 +0530

    Update saj.salexa.py

--- saj.salexa.py
+++ saj.salexa.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 import os
 # 🔐 Configure Gemini
 load_dotenv()
-genai.configure(api_key= os.getenv("API_URL"))
+genai.configure(api_key= os.getenv("GEMINI_API_KEY"))
 model = genai.GenerativeModel("gemini-2.0-flash")
 
 
